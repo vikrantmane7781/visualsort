93899749c85e1592e7f8989f8be4e9b8cd551c20
Nishant Mane <nishantmane39@gmail.com>
2021-02-21 07:55:45 -0800

    First push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,256 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import './AppDark.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+import AppControls from './components/molecules/AppControls';
+import TopBar from './components/organisms/TopBar';
+import AppDrawer from './components/organisms/AppDrawer';
+import SortVisualizer from './components/organisms/SortVisualizer';
+
+import classNames from 'classnames'
+
+import BubbleSort, {
+  BubbleSortKey,
+  BubbleSortDesc
+} from './algorithms/BubbleSort';
+import SelectionSort, {
+  SelectionSortKey,
+  SelectionSortDesc
+} from './algorithms/SelectionSort';
+import InsertionSort, {
+  InsertionSortKey,
+  InsertionSortDesc
+} from './algorithms/InsertionSort';
+import MergeSort, {
+  MergeSortKey,
+  MergeSortDesc
+} from './algorithms/MergeSort';
+
+import HeapSort, {
+  HeapSortKey,
+  HeapSortDesc
+} from './algorithms/HeapSort';
+//custome cursor-------
+
+
+const isMobile = () => {
+  const ua = navigator.userAgent;
+  return /Android|Mobi/i.test(ua);
+}
+
+const Cursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [hidden, setHidden] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const [linkHovered, setLinkHovered] = useState(false);
+
+
+
+
+  useEffect(() => {
+    addEventListeners();
+    handleLinkHoverEvents();
+    return () => removeEventListeners();
+  }, [])
+
+  const addEventListeners = () => {
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseenter", onMouseEnter);
+    document.addEventListener("mouseleave", onMouseLeave);
+    document.addEventListener("mousedown", onMouseDown);
+    document.addEventListener("mouseup", onMouseUp);
+  };
+  const removeEventListeners = () => {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseenter", onMouseEnter);
+    document.removeEventListener("mouseleave", onMouseLeave);
+    document.removeEventListener("mousedown", onMouseDown);
+    document.removeEventListener("mouseup", onMouseUp);
+  }
+  const onMouseLeave = () => {
+    setHidden(true)
+  }
+  const onMouseEnter = () => {
+    setHidden(false)
+  }
+  const onMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  }
+  const onMouseDown = () => {
+    setClicked(true)
+  }
+  const onMouseUp = () => {
+    setClicked(false)
+  }
+  const handleLinkHoverEvents = () => {
+    document.querySelectorAll("a,span,button,h3,h1,td").forEach(el => {
+      el.addEventListener("mouseover", () => setLinkHovered(true));
+      el.addEventListener("mouseout", () => setLinkHovered(false));
+    })
+
+  }
+
+
+
+
+
+  const cursorClasses = classNames(
+    'cursor',
+    {
+      'cursor--clicked': clicked,
+      'cursor--hidden': hidden,
+      'cursor--link-hovered': linkHovered
+    }
   );
+  if (typeof navigator !== 'undefined' && isMobile()) return null;
+  return <div className={cursorClasses}
+    style={{
+      left: `${position.x}px`,
+      top: `${position.y}px`
+    }} />
+}
+class App extends Component {
+  state = {
+    //darkMode: false,
+    array: [],
+    arraySize: 20,
+    trace: [],
+    algorithm: null,
+    appDrawerOpen: false
+  };
+
+  ALGORITHM = {
+    'Bubble Sort': BubbleSort,
+    'Selection Sort': SelectionSort,
+    'Insertion Sort': InsertionSort,
+    'Merge Sort': MergeSort,
+    'Heap Sort': HeapSort,
+  };
+
+  ALGORITHM_KEY = {
+    'Bubble Sort': BubbleSortKey,
+    'Selection Sort': SelectionSortKey,
+    'Insertion Sort': InsertionSortKey,
+    'Merge Sort': MergeSortKey,
+    'Heap Sort': HeapSortKey,
+  };
+
+  ALGORITHM_DESC = {
+    'Bubble Sort': BubbleSortDesc,
+    'Selection Sort': SelectionSortDesc,
+    'Insertion Sort': InsertionSortDesc,
+    'Merge Sort': MergeSortDesc,
+    'Heap Sort': HeapSortDesc,
+  };
+
+  componentDidMount() {
+    this.generateRandomArray();
+  }
+
+  generateRandomArray = () => {
+    // Generate pseudo-random number between 1 and max
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max)) + 1;
+    }
+
+    // Generate an array of length max
+    const array = Array(this.state.arraySize)
+      .fill(0)
+      .map(() => getRandomInt(this.state.arraySize * 5));
+
+    this.setState(
+      {
+        array,
+        trace: []
+      },
+      this.createTrace
+    );
+  };
+
+  handleAlgorithmChange = (algorithm) => {
+    this.setState({ algorithm }, this.generateRandomArray);
+  };
+
+  handleArraySizeChange = (size) => {
+    size = Number(size);
+    size = size > 100 ? 100 : size;
+    size = size < 0 ? 0 : size;
+    this.setState({ arraySize: size }, this.generateRandomArray);
+  };
+
+  createTrace = () => {
+    const numbers = [...this.state.array];
+    const sort = this.ALGORITHM[this.state.algorithm];
+    if (sort) {
+      const trace = sort(numbers);
+      this.setState({ trace });
+    }
+  };
+
+  /*toggleDarkMode = () => {
+    this.setState((prevState) => ({ darkMode: !prevState.darkMode }));
+  };*/
+
+  toggleAppDrawer = () => {
+    this.setState((prevState) => ({
+      appDrawerOpen: !prevState.appDrawerOpen
+    }));
+  };
+
+  render() {
+    let theme = `App`;
+    if (this.state.darkMode) theme += ` App_dark`;
+    if (this.state.appDrawerOpen) theme += ` App_modal_open`;
+
+    const colorKey = this.ALGORITHM_KEY[this.state.algorithm];
+    const desc = this.ALGORITHM_DESC[this.state.algorithm];
+
+    const controls = (
+      <AppControls
+        onGenerateRandomArray={this.generateRandomArray}
+        algorithm={this.state.algorithm}
+        onAlgorithmChange={this.handleAlgorithmChange}
+        arraySize={this.state.arraySize}
+        onArraySizeChange={this.handleArraySizeChange}
+        onToggleDarkMode={this.toggleDarkMode}
+        darkMode={this.state.darkMode}
+      />
+    );
+
+    return (
+
+      <div className={theme}>
+
+        <Cursor />
+        <TopBar
+          drawerOpen={this.state.appDrawerOpen}
+          toggleDrawer={this.toggleAppDrawer}
+        >
+          {controls}
+        </TopBar>
+
+        <AppDrawer
+          open={this.state.appDrawerOpen}
+          closeDrawer={this.toggleAppDrawer}
+        >
+          {controls}
+        </AppDrawer>
+
+        <main className="App__Body">
+          <SortVisualizer
+            array={this.state.array}
+            trace={this.state.trace}
+            colorKey={colorKey}
+            desc={desc}
+          />
+        </main>
+
+      </div>
+
+    );
+  }
 }
 
 export default App;
